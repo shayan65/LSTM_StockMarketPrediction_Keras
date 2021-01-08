cd6d0b03d691e7cc2af5275354366ea99bfbeb6d
shayan65 <shayan.hematiyan@gmail.com>
2021-01-08 00:44:43 -0500

    2nd try

---
+++ readme.md
@@ -0,0 +1,5 @@
+#author: Shayan
+#Recurent Neural Netwrok under Keras:
+#LSTM (LongShortTermMemory):
+
+
---
+++ rnn.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
